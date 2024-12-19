8519f39d221d7ccab45598fc0c1bdbb2dece99d0
IdoiaBE <1992ibe@gmail.com>
2024-12-19 19:07:15 +0100

    landing js partial removed

--- src/js/2_landing.js
+++
@@ -1,9 +0,0 @@
-// Quiero que cuando el tamaño de la pantalla pase de 768px, la imagen de diseña, rellena y comparte desaparezca y aparezcan otras 3 imágenes.
-
-//Pasos:
-//1. Llamar a los elementos que voy a escuchar y modificar.
-//2. Escuchar el cambio en el tamaño de la pantalla. -- ??  
-//     let screenWidth = screen.availWidth;  --> me da el ancho de la pantalla y lo guardo en una variable
-//3. Programar la respuesta. -- Poner la clase hidden al primer artículo y quitarsela a los otros 3 artículos. 
-
-
