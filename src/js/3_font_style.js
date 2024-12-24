5e31f42a1bcf7966def130bbe6afed6c07f7d248
MelsCrest <melisacresta@gmail.com>
2024-12-24 11:56:42 +0100

    font-default added in select option font-style JS

--- src/js/3_font_style.js
+++ src/js/3_font_style.js
@@ -3,23 +3,32 @@
 const fontOption = document.querySelector('.js-fontOp');
 const cardFont = document.querySelector('.js-card-font');
 
-function handleInputFont (event) {
+function handleInputFont () {
 
     if(fontOption.value === 'option1') {
     cardFont.classList.add('fontOp1');
-    cardFont.classList.remove('fontOp2', 'fontOp3');
+    cardFont.classList.remove('fontOp0', 'fontOp2', 'fontOp3');
     }
 
     else if(fontOption.value === 'option2') {
     cardFont.classList.add('fontOp2');
-    cardFont.classList.remove('fontOp1', 'fontOp3');
+    cardFont.classList.remove('fontOp0', 'fontOp1', 'fontOp3');
     }
 
     else if(fontOption.value === 'option3') {
-    cardFont.classList.add('colorOp3');
-    cardFont.classList.remove('fontOp1', 'fontOp3');
+    cardFont.classList.add('fontOp3');
+    cardFont.classList.remove('fontOp0', 'fontOp1', 'fontOp2');
         }
+
+    else if(fontOption.value === 'font-default') {
+        cardFont.classList.add('fontOp0');
+        cardFont.classList.remove('fontOp1', 'fontOp2', 'fontOp3');
+    }
     
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    handleInputFont();
+});
 
 fontOption.addEventListener('input', handleInputFont);
