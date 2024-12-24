5e4924ad4671b06fecebd33657a43d9e70883a6c
MelsCrest <melisacresta@gmail.com>
2024-12-24 11:41:42 +0100

    section-form HTML, color-themes JS and color-options CSS updated

--- src/js/1_color_themes.js
+++ src/js/1_color_themes.js
@@ -3,37 +3,33 @@
 const colorOption = document.querySelector('.js-colorOp');
 const cardColor = document.querySelector('.js-card-color');
 
-function handleInputColor (event) {
-    // const id = event.target.id;
-    // const value = event.target.value;
-
-    // console.log(target);
-
-    // console.log(value);
-
+function handleInputColor() {
+    
     if(colorOption.value === 'option1') {
-    cardColor.classList.add('colorOp1');
-    cardColor.classList.remove('colorOp0','colorOp2', 'colorOp3', 'colorOp4');
+        cardColor.classList.add('colorOp1');
+        cardColor.classList.remove('colorOp0','colorOp2', 'colorOp3', 'colorOp4');
     }
-
     else if(colorOption.value === 'option2') {
-    cardColor.classList.add('colorOp2');
-    cardColor.classList.remove('colorOp0','colorOp1', 'colorOp3', 'colorOp4');
+        cardColor.classList.add('colorOp2');
+        cardColor.classList.remove('colorOp0','colorOp1', 'colorOp3', 'colorOp4');
     }
-
     else if(colorOption.value === 'option3') {
-    cardColor.classList.add('colorOp3');
-    cardColor.classList.remove('colorOp0','colorOp1', 'colorOp2', 'colorOp4');
-        }
+        cardColor.classList.add('colorOp3');
+        cardColor.classList.remove('colorOp0','colorOp1', 'colorOp2', 'colorOp4');
+    }
     else if(colorOption.value === 'option4') {
         cardColor.classList.add('colorOp4');
         cardColor.classList.remove('colorOp0','colorOp1', 'colorOp2', 'colorOp3');
-        }
-    else {
+    }
+    else if(colorOption.value === 'color-default'){
         cardColor.classList.add('colorOp0');
         cardColor.classList.remove('colorOp1', 'colorOp2', 'colorOp3', 'colorOp4');
-    }
-    
+    }   
 }
+
+/*color-default is shown at the beginning*/
+document.addEventListener('DOMContentLoaded', () => {
+    handleInputColor();
+});
 
 colorOption.addEventListener('input', handleInputColor);
