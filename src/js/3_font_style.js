4edcc39d548543a7d5ea879d059379a3f5410091
MelsCrest <melisacresta@gmail.com>
2024-12-26 14:44:12 +0100

    modified font-javascript for font-selection

--- src/js/3_font_style.js
+++ src/js/3_font_style.js
@@ -1,44 +1,43 @@
 'use strict';
 
 const fontOption = document.querySelector('.js-fontOp');
-const cardFont = document.querySelector('.js-card-font');
-const cardTutoring = document.querySelector('.js-card-font-item1');
-const cardQueen = document.querySelector('.js-card-font-item2');
-const cardDiscover= document.querySelector('.js-card-font-item3');
-const cardCode = document.querySelector('.js-card-font-item4');
+const cardFont = document.querySelectorAll('.js-card-font');  
 
-
-function handleInputFont () {
-
-    if(fontOption.value === 'option1') {
-    cardFont.classList.add('fontOp1');
-    cardFont.classList.remove('fontOp0', 'fontOp2', 'fontOp3');
-    cardTutoring.classList.add('fontOp1');
-    cardTutoring.classList.remove('fontOp0', 'fontOp2', 'fontOp3');
-    cardQueen.classList.add('fontOp1');
-    cardQueen.classList.remove('fontOp0', 'fontOp2', 'fontOp3');
-    cardDiscover.classList.add('fontOp1');
-    cardDiscover.classList.remove('fontOp0', 'fontOp2', 'fontOp3');
-    cardCode.classList.add('fontOp1');
-    cardCode.classList.remove('fontOp0', 'fontOp2', 'fontOp3');
-    }
-
-    else if(fontOption.value === 'option2') {
-    cardFont.classList.add('fontOp2');
-    cardFont.classList.remove('fontOp0', 'fontOp1', 'fontOp3');
-    }
-
-    else if(fontOption.value === 'option3') {
-    cardFont.classList.add('fontOp3');
-    cardFont.classList.remove('fontOp0', 'fontOp1', 'fontOp2');
-        }
-
-    else if(fontOption.value === 'font-default') {
-        cardFont.classList.add('fontOp0');
-        cardFont.classList.remove('fontOp1', 'fontOp2', 'fontOp3');
-    }
+function handleInputFont() {
     
-}
+    for (let index in cardFont) { // Checks if the property is a numeric index (must be a node of a NodeList).
+        if (cardFont.hasOwnProperty(index)) {
+            cardFont[index].classList.remove('fontOp0', 'fontOp1', 'fontOp2', 'fontOp3');
+        }
+        //The hasOwnProperty() method returns a boolean indicating whether the object has the specified property.
+    }
+
+    if (fontOption.value === 'option1') {
+        for (let index in cardFont) {
+            if (cardFont.hasOwnProperty(index)) {
+                cardFont[index].classList.add('fontOp1');
+            }
+        }
+    } else if (fontOption.value === 'option2') {
+        for (let index in cardFont) {
+            if (cardFont.hasOwnProperty(index)) {
+                cardFont[index].classList.add('fontOp2');
+            }
+        }
+    } else if (fontOption.value === 'option3') {
+        for (let index in cardFont) {
+            if (cardFont.hasOwnProperty(index)) {
+                cardFont[index].classList.add('fontOp3');
+            }
+        }
+    } else if (fontOption.value === 'font-default') {
+        for (let index in cardFont) {
+            if (cardFont.hasOwnProperty(index)) {
+                cardFont[index].classList.add('fontOp0');
+            }
+        }
+    }
+};
 
 // Run the function on page load to ensure that the class fontOp0 is applied by default.
 document.addEventListener('DOMContentLoaded', () => {
@@ -46,3 +45,5 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 fontOption.addEventListener('input', handleInputFont);
+
+
