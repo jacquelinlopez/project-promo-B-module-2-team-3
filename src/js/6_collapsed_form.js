11e8f77185362076b78c367e7a001a56b1beba6b
MelsCrest <melisacresta@gmail.com>
2025-01-10 19:36:13 +0100

    changeArrow function created

--- src/js/6_collapsed_form.js
+++ src/js/6_collapsed_form.js
@@ -1,25 +1,47 @@
 "use strict";
+//fieldsets
 const fieldsetDesign = document.querySelector(".js-fieldset-design");
 const fieldsetFill = document.querySelector(".js-fieldset-fill");
 const fieldsetCreate = document.querySelector(".js-fieldset-create");
+//btns
 const btnDesignCol = document.querySelector('.js-design-collapse');
 const btnFillCol = document.querySelector('.js-fill-collapse');
 const btnCreateCol = document.querySelector('.js-create-collapse');
+//spans
+const spanDesign = document.querySelector(".js-arrow1");
+const spanFill = document.querySelector(".js-arrow2");
+const spanCreate = document.querySelector(".js-arrow3");
 
+// handClick functions
 function handleClickDesign(){
     AddClass(fieldsetDesign, 'collapsed-form');
+    changeArrow(fieldsetDesign, spanDesign);
 };
 function handleClickFill(){
     AddClass(fieldsetFill, 'collapsed-form');
+    changeArrow(fieldsetFill, spanFill);
 };
 function handleClickCreateCol(){
     AddClass(fieldsetCreate, 'collapsed-form');
+    changeArrow(fieldsetCreate, spanCreate);
 };
 
+// addClass function
 function AddClass(tag, clase){
         tag.classList.toggle(clase);
 };
 
+// changeArrow function
+function changeArrow(tag, span){
+    const isCollapsed = tag.classList.contains('collapsed-form');
+    if(isCollapsed){
+        span.innerHTML = "⪢";
+    } else {
+        span.innerHTML = "⩔";
+    };
+};
+
+// eventListeners
 btnDesignCol.addEventListener('click', handleClickDesign);
 btnFillCol.addEventListener('click', handleClickFill);
 btnCreateCol.addEventListener('click', handleClickCreateCol);
